b32bdf74120693eff5db5825aff8617e741c0690
Ramesh <vanaparthiramesh741@gmail.com>
2024-03-17 10:49:16 +0530

    17 March

---
+++ model.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose')
+
+const abc = mongoose.Schema({
+   restarent_name: {type:String , required: true,},
+   description: {type:String , required: true,},
+   reviews : {type: String, required: true,},
+   location : {type: String, required: true,},
+   available : {type: String, required: true,},
+   image : {type:String, required:false,}       
+})
+
+
+module.exports = mongoose.model("Restarent",abc)
---
+++ server.js
@@ -0,0 +1,53 @@
+const express = require('express')
+const dotEnv = require('dotenv')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const app = express()
+const Restarent = require('./model')
+
+dotEnv.config()
+const PORT = process.env.PORT || 8000
+
+app.use(express.json())
+
+app.use(cors())
+
+mongoose.connect(process.env.mango_Url)
+.then(() => {
+   console.log("Mongodb Connected Successfully!")       
+})
+.catch((error) => {
+    console.log('Error',error)      
+})
+
+//const PORT = 8000;
+app.listen(PORT, () => {
+   console.log(`Server is running at localhost ${PORT}`)       
+})
+
+app.get('/', (req,res) => {
+   res.send(`<h1>Hello Ramesh`)       
+})
+
+app.post('/add-restarent', async (req,res) => {
+   let con = req.body;
+   const {restarent_name,description,reviews,available,location,image} = con       
+   try {
+      const newData = new Restarent({restarent_name,description,reviews,location,available,image});
+      await newData.save();
+      //return res.json(await Restarent.find())
+      res.status(201).json(newData)    
+   } catch (error) {
+     console.log(error.message)     
+     //res.status(500).json({message: 'server Error})
+   }       
+})
+
+app.get('/restarents', async (req,res) => {
+   try {
+       const allData = await Restarent.find();
+       return res.json(allData) ;  
+   } catch (error) {
+      console.log(error.message)    
+   }       
+})
